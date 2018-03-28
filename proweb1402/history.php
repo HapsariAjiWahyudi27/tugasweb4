b352bad5299ae6abcf93b6e75f3e2bf7fb46bb96
HapsariAjiWahyudi27 <37051162+HapsariAjiWahyudi27@users.noreply.github.com>
2018-03-28 17:28:17 +0700

    Add files via upload

---
+++ proweb1402/history.php
@@ -0,0 +1,17 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <hr>
+<table width="100" height="100" border="1" align="center">
+    <tr>
+        <td rowspan="1"><img src="asset/images/1.jpg" width="200" height="200"></td>
+        <td rowspan="1"><img src="asset/images/2.jpg" width="200" height="200"></td>
+        <td rowspan="1"><img src="asset/images/3.jpg" width="200" height="200"></td>
+        
+    </tr>
+</table>
+    </body>
